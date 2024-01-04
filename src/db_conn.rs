10e8042ae56d833ed9ec49c49e2a932e1ea88650
ahaocoder <ahao.coder@gmail.com>
2024-01-04 19:39:23 +0800

    2

---
+++ src/db_conn.rs
@@ -0,0 +1,10 @@
+use sqlx::mysql::MySqlPoolOptions;
+
+pub async fn db_conn() -> Result<sqlx::MySqlPool, sqlx::Error> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://root:12345678@localhost/school")
+        .await?;
+
+    Ok(pool)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use rocket::tokio;
-use sqlx::mysql::MySqlPoolOptions;
-// use sqlx::mysql::MySqlPoolOptions;
-// etc.
+mod db_conn;
+mod models;
+mod stu_lib;
 
-// #[async_std::main] // Requires the `attributes` feature of `async-std`
-#[tokio::main]
-// or #[actix_web::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // Create a connection pool
-    //  for MySQL, use MySqlPoolOptions::new()
-    //  for SQLite, use SqlitePoolOptions::new()
-    //  etc.
-    let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect("mysql://root:12345678@localhost/school").await?;
+#[macro_use] extern crate rocket;
 
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    let row: (i64,) = sqlx::query_as("SELECT id from posts where title = ?")
-        .bind("2")
-        .fetch_one(&pool).await?;
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
-    println!("{:?}", row);
-
-    Ok(())
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
